01dbee6b660fc3764b258409578ee8ea93f560f4
John McCabe <john@johnmccabe.net>
2017-09-13 01:07:47 +0100

    Refactor Cmd and Style and adds GoDocs

--- bitbar.go
+++ bitbar.go
@@ -5,13 +5,14 @@ import (
 	"strings"
 )
 
-// Menu TODO
-type Menu struct {
-	MenuBar  MenuBar
-	DropDown *DropDown
+// Plugin holds the content of the Bitbar plugin, lines and submenus.
+type Plugin struct {
+	StatusBar StatusBar
+	SubMenu   *SubMenu
 }
 
-// Line TODO
+// Line holds the content, styling and behaviour of a line in a Bitbar
+// menu, both in the menu and submenus
 type Line struct {
 	text          string
 	href          string
@@ -27,233 +28,284 @@ type Line struct {
 	emojize       *bool
 	ansi          *bool
 	bash          string
-	param         []string
+	params        []string
 	templateImage string
 	image         string
 }
 
-// Style TODO
+// Style wraps options related to text presentation which can be added to a line
+// using the *line.Style(s Style) function.
 type Style struct {
-	Color string
-	Font  string
-	Size  int
+	Color   string
+	Font    string
+	Size    int
+	Length  int
+	Trim    bool
+	Emojize bool
+	Ansi    bool
 }
 
-// Options TODO
-type Options struct {
-	Terminal  bool
-	Refresh   bool
-	DropDown  bool
-	Length    int
-	Trim      bool
-	Alternate bool
-	Emojize   bool
-	Ansi      bool
-}
-
-// Cmd TODO
+// Cmd wraps options related to commands which can be added to a line using the
+// *line.Command(c Cmd) function.
 type Cmd struct {
-	Bash  string
-	Param []string
+	Bash     string
+	Params   []string
+	Terminal bool
+	Refresh  bool
 }
 
-// MenuBar TODO
-type MenuBar struct {
-	Line []*Line
+// StatusBar holds one of more Lines of text which are rendered in the status bar.
+// Multiple Lines will be cycled through over and over
+type StatusBar struct {
+	Lines []*Line
 }
 
-// DropDownItem TODO
-type DropDownItem interface{}
+// SubMenuItem is used to hold a Line or SubMenu.
+type SubMenuItem interface{}
 
-// DropDown TODO
-type DropDown struct {
+// SubMenu contains a slice of SubMenuItems which can be Lines or additional
+// SubMenus. The Level indicates how nested the submenu is which is used during
+// render to prepend the correct number of `--` prefixes.
+type SubMenu struct {
 	Level int
-	Line  []DropDownItem
+	Lines []SubMenuItem
 }
 
 // New returns an empty Bitbar menu without any context
-func New() Menu {
-	return Menu{}
+func New() Plugin {
+	return Plugin{}
 }
 
-// AddMenuBar TODO
-func (b *Menu) AddMenuBar(s string) *Line {
+// StatusLine creates a line adding text to the status bar which will be added
+// before the main dropdown delimiter (`---`), multiple StatusLines will be
+// cycled through over and over.
+//  *menu.StatusLine("Text for the status bar")
+func (p *Plugin) StatusLine(s string) *Line {
 	l := new(Line)
 	l.text = s
-	b.MenuBar.Line = append(b.MenuBar.Line, l)
+	p.StatusBar.Lines = append(p.StatusBar.Lines, l)
 	return l
 }
 
-// AddDropDown TODO
-func (b *Menu) AddDropDown() *DropDown {
-	b.DropDown = new(DropDown)
-	b.DropDown.Level = 0
-	return b.DropDown
+// NewSubMenu creates a submenu off the main menu.
+//  *menu.NewSubMenu()
+func (p *Plugin) NewSubMenu() *SubMenu {
+	p.SubMenu = new(SubMenu)
+	p.SubMenu.Level = 0
+	return p.SubMenu
 }
 
-// Add TODO
-func (d *DropDown) Add(s string) *Line {
+// Line creates a line adding text to the dropdown which will be added after
+// the main dropdown delimiter (`---`).
+//  submenu.Line("Submenu item text")
+func (d *SubMenu) Line(s string) *Line {
 	l := new(Line)
 	l.text = s
-	d.Line = append(d.Line, l)
+	d.Lines = append(d.Lines, l)
 	return l
 }
 
-// AddDropDown TODO
-func (d *DropDown) AddDropDown() *DropDown {
-	newDropDown := new(DropDown)
-	newDropDown.Level = d.Level + 1
-	d.Line = append(d.Line, newDropDown)
-	return newDropDown
+// NewSubMenu creates a nested submenu off a submenu.
+//  submenu.NewSubMenu()
+func (d *SubMenu) NewSubMenu() *SubMenu {
+	newSubMenu := new(SubMenu)
+	newSubMenu.Level = d.Level + 1
+	d.Lines = append(d.Lines, newSubMenu)
+	return newSubMenu
 }
 
-// Style TODO
+// Style provides a alternate method for setting the text style related
+// options.
+//  style := bitbar.Style{
+//    Color:   "red",
+//    Font:    "UbuntuMono-Bold",
+//    Size:    14,
+//    Length:  20,
+//    Trim:    false,
+//    Emojize: false,
+//    Ansi:    false,
+//  }
+//  line.Style(false)
 func (l *Line) Style(s Style) *Line {
 	l.color = s.Color
 	l.font = s.Font
 	l.size = s.Size
+	l.length = s.Length
+	l.trim = &s.Trim
+	l.emojize = &s.Emojize
+	l.ansi = &s.Ansi
 	return l
 }
 
-// Options TODO
-func (l *Line) Options(o Options) *Line {
-	l.terminal = &o.Terminal
-	l.refresh = &o.Refresh
-	l.dropDown = &o.DropDown
-	l.length = o.Length
-	l.trim = &o.Trim
-	l.alternate = &o.Alternate
-	l.emojize = &o.Emojize
-	l.ansi = &o.Ansi
-	return l
-}
-
-// Command TODO
+// Command provides a alternate method for setting the bash script and
+// params along with some related flags via a Command struct.
+//  cmd := bitbar.Cmd{
+//    Bash:     "/Users/user/BitBar_Plugins/scripts/nginx.restart.sh",
+//    Params:   []string{"--verbose"},
+//    Terminal: false,
+//    Refresh:  true,
+//  }
+//  line.Command(cmd)
 func (l *Line) Command(c Cmd) *Line {
 	l.bash = c.Bash
-	l.param = c.Param
+	l.params = c.Params
+	l.terminal = &c.Terminal
+	l.refresh = &c.Refresh
 	return l
 }
 
-// Href TODO
+// Href adds a URL to the line and makes it clickable.
+//  line.Href("http://github.com/johnmccabe/bitbar")
 func (l *Line) Href(s string) *Line {
 	l.href = s
 	return l
 }
 
-// Color TODO
+// Color sets the lines font color, can take a name or hex value.
+//  line.Color("red")
+//  line.Color("#ff0000")
 func (l *Line) Color(s string) *Line {
 	l.color = s
 	return l
 }
 
-// Font TODO
+// Font sets the lines font.
+//  line.Font("UbuntuMono-Bold")
 func (l *Line) Font(s string) *Line {
 	l.font = s
 	return l
 }
 
-// Size TODO
+// Size sets the lines font size.
+//  line.Size(12)
 func (l *Line) Size(i int) *Line {
 	l.size = i
 	return l
 }
 
-// Bash TODO
+// Bash makes makes the line clickable and adds a script that will be run
+// on click.
+//  line.Bash("/Users/user/BitBar_Plugins/scripts/nginx.restart.sh")
 func (l *Line) Bash(s string) *Line {
 	l.bash = s
 	return l
 }
 
-// Param TODO
-func (l *Line) Param(s []string) *Line {
-	l.param = s
+// Params adds arguments which are passed to the script specified by *Line.Bash().
+//  args := []string{"--verbose"}
+//  line.Bash("/Users/user/BitBar_Plugins/scripts/nginx.restart.sh").Params(args)
+func (l *Line) Params(s []string) *Line {
+	l.params = s
 	return l
 }
 
-// Terminal TODO
+// Terminal sets a flag which controls whether a Terminal is opened when the bash
+// script is run.
+//  line.Bash("/Users/user/BitBar_Plugins/scripts/nginx.restart.sh").Terminal(true)
 func (l *Line) Terminal(b bool) *Line {
 	l.terminal = &b
 	return l
 }
 
-// Refresh TODO
+// Refresh sets a flag which controls whether clicking the line results in the plugin
+// being refreshed. If the line has a bash script attached then the plugin is refreshed
+// after the script finishes.
+//  line.Bash("/Users/user/BitBar_Plugins/scripts/nginx.restart.sh").Refresh(true)
+//  line.Refresh(true)
 func (l *Line) Refresh(b bool) *Line {
 	l.refresh = &b
 	return l
 }
 
-// DropDown TODO
+// DropDown sets a flag which controls whether the line only appears and cycles in the
+// status bar but not in the dropdown.
+//  line.DropDown(false)
 func (l *Line) DropDown(b bool) *Line {
 	l.dropDown = &b
 	return l
 }
 
-// Length TODO
+// Length truncates the line after the specified number of characters. An elipsis will
+// be added to any truncated strings, as well as a tooltip displaying the full string.
+//  line.DropDown(false)
 func (l *Line) Length(i int) *Line {
 	l.length = i
 	return l
 }
 
-// Trim TODO
+// Trim sets a flag to control whether leading/trailing whitespace is trimmed from the
+// title. Defaults to `true`.
+//  line.Trim(false)
 func (l *Line) Trim(b bool) *Line {
 	l.trim = &b
 	return l
 }
 
-// Alternate TODO
+// Alternate sets a flag to mark a line as an alternate to the previous one for when the
+// Option key is pressed in the dropdown.
+//  line.Alternate(false)
 func (l *Line) Alternate(b bool) *Line {
 	l.alternate = &b
 	return l
 }
 
-// TemplateImage TODO
+// TemplateImage sets an image for the line. The image data must be passed as base64
+// encoded string and should consist of only black and clear pixels. The alpha channel
+// in the image can be used to adjust the opacity of black content, however. This is the
+// recommended way to set an image for the statusbar. Use a 144 DPI resolution to support
+// Retina displays. The imageformat can be any of the formats supported by Mac OS X.
+//  line.TemplateImage("iVBORw0KGgoAAAANSUhEUgAAA...")
 func (l *Line) TemplateImage(s string) *Line {
 	l.templateImage = s
 	return l
 }
 
-// Image TODO
+// Image set an image for the line. The image data must be passed as base64 encoded
+// string. Use a 144 DPI resolution to support Retina displays. The imageformat can be any
+// of the formats supported by Mac OS X
+//  line.Image("iVBORw0KGgoAAAANSUhEUgAAA...")
 func (l *Line) Image(s string) *Line {
 	l.image = s
 	return l
 }
 
-// Emojize TODO
+// Emojize sets a flag to control parsing of github style :mushroom: into 🍄.
+//  line.Emojize(false)
 func (l *Line) Emojize(b bool) *Line {
 	l.emojize = &b
 	return l
 }
 
-// Ansi TODO
+// Ansi sets a flag to control parsing of ANSI codes.
+//  line.Ansi(false)
 func (l *Line) Ansi(b bool) *Line {
 	l.ansi = &b
 	return l
 }
 
-// Render TODO
-func (b *Menu) Render() string {
+// Render the Bitbar menu as a string.
+func (p *Plugin) Render() string {
 	var output string
-	for _, line := range b.MenuBar.Line {
+	for _, line := range p.StatusBar.Lines {
 		output = output + fmt.Sprintf("%s\n", renderLine(line))
 	}
 	output = output + "---\n"
-	output = output + renderDropDown(b.DropDown)
+	output = output + renderSubMenu(p.SubMenu)
 	return output
 }
 
-func renderDropDown(d *DropDown) string {
+func renderSubMenu(d *SubMenu) string {
 	var output string
 	var prefix string
 	if d.Level > 0 {
 		prefix = strings.Repeat("--", d.Level) + " "
 	}
-	for _, line := range d.Line {
+	for _, line := range d.Lines {
 		switch v := line.(type) {
 		case *Line:
 			output = output + fmt.Sprintf("%s%s\n", prefix, renderLine(v))
-		case *DropDown:
-			output = output + renderDropDown(v)
+		case *SubMenu:
+			output = output + renderSubMenu(v)
 		}
 	}
 	return output
@@ -277,8 +329,8 @@ func renderLine(l *Line) string {
 	if l.bash != "" {
 		options = append(options, fmt.Sprintf("bash=\"%s\"", l.bash))
 	}
-	if len(l.param) > 0 {
-		for i, param := range l.param {
+	if len(l.params) > 0 {
+		for i, param := range l.params {
 			options = append(options, fmt.Sprintf("param%d=%s", i+1, param))
 		}
 	}
--- example/timezones/main.go
+++ example/timezones/main.go
@@ -14,18 +14,19 @@ func main() {
 		Size:  12,
 	}
 	c := bitbar.Cmd{
-		Bash:  "/path/to/cmd",
-		Param: []string{"arg1", "arg2"},
+		Bash:   "/path/to/cmd",
+		Params: []string{"arg1", "arg2"},
 	}
-	b.AddMenuBar("MenuItem 1").Href("http://localhost:8080").Color("red").DropDown(false)
-	b.AddMenuBar("MenuItem 2").DropDown(false)
-	dropdown := b.AddDropDown()
-	dropdown.Add("DropDown Level 1 A").Style(s)
-	subdrop := b.DropDown.AddDropDown()
-	subdrop.Add("DropDown Level 2 A")
-	dropdown.Add("DropDown Level 1 B").Command(c)
-	subsubdrop := subdrop.AddDropDown()
-	subsubdrop.Add("DropDown Level 3 A")
+	b.StatusLine("MenuItem 1").Href("http://localhost:8080").Color("red").DropDown(false)
+	b.StatusLine("MenuItem 2").DropDown(false)
+	menu := b.NewSubMenu()
+	menu.Line("DropDown Level 1 A").Style(s)
+	submenu := b.SubMenu.NewSubMenu()
+	submenu.Line("DropDown Level 2 A")
+	submenu.Line("DropDown Level 1 B").Command(c)
+	subsubmenu := submenu.NewSubMenu()
+	subsubmenu.Line("DropDown Level 3 A")
+	menu.Line("DropDown Level 1 B").Style(s)
 
 	fmt.Print(b.Render())
 }
